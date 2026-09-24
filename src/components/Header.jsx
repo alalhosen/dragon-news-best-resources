e1c8f0780f1c9d01e5ee016a07aeadab43c3dfb3
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-24 13:18:34 +0600

    create homeLayout & add marquee dynamic date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { format } from "date-fns";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-5">
       <img src={logo} alt="" />
-      <p>Journalism Without Fear or Favour</p>
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p>{format(new Date(),"EEEE, MMMM MM, yyyy")}</p>
     </div>
   );
 };
