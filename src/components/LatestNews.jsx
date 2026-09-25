b1104e99c152f3043f0e55453f1cf437107310dd
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-25 22:13:44 +0600

    add navbar & login button with user

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,15 +1,30 @@
-import Marquee from "react-fast-marquee";
+import MarqueeModule from "react-fast-marquee";
+
+const Marquee = MarqueeModule.default;
 
 const LatestNews = () => {
+
+
   return (
     <div className="flex gap-5 items-center bg-base-200">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
-      <Marquee>
-
-      <p className="font-bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-      </p>
+      {/* <Marquee> */}
+      <Marquee className="flex gap-5" pauseOnHover={true}>
+          <p className="font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        </p>
+          <p className="font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        </p>
+          <p className="font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        </p>
+          <p className="font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        </p>
       </Marquee>
+      
+      {/* </Marquee> */}
     </div>
   );
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+import LeftAside from "../components/homeLayout/LeftAside";
+import RightAside from "../components/homeLayout/RightAside";
 
 const HomeLayout = () => {
   return (
@@ -8,15 +11,20 @@ const HomeLayout = () => {
       <header>
         <Header></Header>
         <section className="w-11/12 mx-auto my-3">
-            <LatestNews></LatestNews>
+          <LatestNews></LatestNews>
         </section>
+        <Navbar></Navbar>
       </header>
       <main>
-        <section className="left-nav"></section>
+        <aside>
+          <LeftAside></LeftAside>
+        </aside>
         <section className="main">
           <Outlet></Outlet>
         </section>
-        <section className="right-nav"></section>
+  <aside>
+    <RightAside></RightAside>
+  </aside>
       </main>
     </div>
   );
